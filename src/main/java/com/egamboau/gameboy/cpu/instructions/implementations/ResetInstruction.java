36be60219fbe2f23d812a45979013edbe65a6a26
Eduardo Gamboa <egamboau@gmail.com>
2026-08-29 10:28:13 -0600

    style(cpu): fix reset checkstyle errors

--- src/main/java/com/egamboau/gameboy/cpu/instructions/implementations/ResetInstruction.java
+++ src/main/java/com/egamboau/gameboy/cpu/instructions/implementations/ResetInstruction.java
@@ -1,7 +1,6 @@
 package com.egamboau.gameboy.cpu.instructions.implementations;
 
 import com.egamboau.gameboy.cpu.CPU;
-import com.egamboau.gameboy.cpu.instructions.AddressMode;
 import com.egamboau.gameboy.cpu.instructions.Instruction;
 import com.egamboau.gameboy.cpu.instructions.RegisterType;
 import com.egamboau.gameboy.memory.BitMasks;
@@ -12,9 +11,14 @@ public class ResetInstruction extends Instruction {
 
     private int vector;
 
-    public ResetInstruction(final int vector) {
+    /**
+     * Creates a reset instruction for the specified vector.
+     *
+     * @param currentVector reset vector address
+     */
+    public ResetInstruction(final int currentVector) {
         super(null, null, null);
-        this.vector = vector;
+        this.vector = currentVector;
     }
 
     @Override
